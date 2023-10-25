d442fe751453220f966074fb697cbf99e5b4322d
Martin老師 <tpemartin@gmail.com>
2023-10-25 15:46:24 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+
+const outDir = resolve(__dirname, 'docs')
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/week7-web-app/',
+  build: {
+    outDir,
+    emptyOutDir: true
+}})
